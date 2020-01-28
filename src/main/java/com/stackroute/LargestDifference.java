5d38a36f2b7ea0e0d2ad4e62d148cfb5ae7dbb8b
ubuntu <ubuntu@ip-172-31-32-139.ec2.internal>
2020-01-28 06:32:17 +0000

    Logical Assignment 5

--- src/main/java/com/stackroute/LargestDifference.java
+++ src/main/java/com/stackroute/LargestDifference.java
@@ -1,16 +1,58 @@
 package com.stackroute;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class LargestDifference {
 
    	//Do not print anything other than what's expected/asked in problem
     public static void main(String[] args) {
    	//Use Scanner to get input from console
-
+    	Scanner sc = new Scanner(System.in);
+    	int total = sc.nextInt();
+    	int [] numArray = new int[total];
+    	for (int i = 0; i < total; i++) {
+    		numArray[i] = sc.nextInt();
+		}
+    	
+    	int index = findLargestDifference(numArray);
+    	System.out.println(index);
 
 	}
 
 	public static int findLargestDifference(int[] numbers) {
-		return -2;
+		int max =0;
+		int resIndex = -1;
+		
+		if(numbers!= null && numbers.length>0) {
+			for (int i = 0; i < numbers.length-1; i++) {
+				int diff = Math.abs(numbers[i] - numbers[i+1]);
+				if(max<diff) {
+					max = diff;
+					if(numbers[i] < numbers[i+1]) {
+						resIndex = i+1;
+					}else {
+						resIndex = i;
+					}
+				}else if (max == diff) {
+					if(numbers[i] > numbers[i+1]) {
+						if(numbers[resIndex] < numbers[i]) {
+							resIndex = i;
+						}
+						
+					}else {
+						
+						if(numbers[resIndex] < numbers[i]) {
+							resIndex = i+1;
+						}
+					}
+					
+				}
+			}
+		
+			
+		}
+		return resIndex;
 	}
 
 }
